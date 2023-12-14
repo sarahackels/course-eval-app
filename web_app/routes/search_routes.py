483a3f8e48297f107a84ab7c7cc99035ff1d5928
sarahackels <107145319+sarahackels@users.noreply.github.com>
2023-12-14 18:24:29 -0500

    dashboard w chart works

--- web_app/routes/search_routes.py
+++ web_app/routes/search_routes.py
@@ -4,6 +4,7 @@
 from flask import Blueprint, request, render_template, redirect, flash
 
 from app.search_prof import sortdata
+from app.search_prof import getmean
 
 search_routes = Blueprint("search_routes", __name__)
 
@@ -25,39 +26,17 @@ def search_dashboard():
         request_data = dict(request.args)
         print("URL PARAMS:", request_data)
 
-    name = request_data.get("lastname") or "Rossetti"
+    profname = request_data.get("lastname") or "Rossetti"
 
     try:
-        #df = sortdata(name)
+        df = sortdata(name=profname)
+        profstats = getmean(df)
         
         flash("Searched for Professor Ratings!", "success")
 
-        return render_template("search_dashboard.html", name=name)
+        return render_template("search_dashboard.html", name=profname,data=df, stats=profstats)
     except Exception as err:
         print('OOPS', err)
 
         flash("Please check your Professor Name and try again!", "danger")
         return redirect("/search/form")
-
-#
-# API ROUTES
-#
-'''
-@stocks_routes.route("/api/stocks.json")
-def stocks_api():
-    print("STOCKS DATA (API)...")
-
-    # for data supplied via GET request, url params are in request.args:
-    url_params = dict(request.args)
-    print("URL PARAMS:", url_params)
-    symbol = url_params.get("symbol") or "NFLX"
-
-    try:
-        df = fetch_stocks_data(symbol=symbol)
-        data = df.to_dict("records")
-        return {"symbol": symbol, "data": data }
-    except Exception as err:
-        print('OOPS', err)
-        return {"message":"Market Data Error. Please try again."}, 404
-
-'''
