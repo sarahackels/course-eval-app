06d0aebaff170db05d19168fbe35fb513d2d800b
sarahackels <107145319+sarahackels@users.noreply.github.com>
2023-12-14 19:37:01 -0500

    minor cleanup and fail at dropdown

---
+++ app/all_professors.py
@@ -0,0 +1,11 @@
+from app.get_data_online import get_data2
+
+def unique_instructors():
+    our_df = get_data2()
+    profs = our_df["Instructor Name"].sort_values().unique().tolist()
+    print(len(profs))
+    return profs
+
+
+if __name__ == "__main__":
+    unique_instructors()
